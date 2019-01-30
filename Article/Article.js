d47ecd508738d7e8b5079d5c6eba572dafe76d76
Pav0l <pavol.karas@gmail.com>
2019-01-30 14:28:38 +0100

    Stretch goal - add close button

--- Article/Article.js
+++ Article/Article.js
@@ -10,25 +10,36 @@ class Article {
     this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', this.expandArticle.bind(this));
+
+    //Close button
+    this.closeButton = this.domElement.querySelector('.closeButton');
+    this.closeButton.textContent = 'close';
+    this.closeButton.addEventListener('click', this.closeArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    for (let i = 0; i < articles.length; i++) {
+      console.log(articles[i]);
+      articles[i].classList.remove('article-open');      
+    }
     this.domElement.classList.toggle('article-open');
+  }
+  closeArticle() {
+    this.domElement.classList.remove('article-open');
   }
 }
 
-/* START HERE: 
-
+/* START HERE:
 - Select all classes named ".article" and assign that value to the articles variable.  
 
 - With your selection in place, now chain .forEach() on to the articles variable 
 to iterate over the articles NodeList and create a new instance of Article by passing 
 in each article as a parameter to the constructor.
-
 */
+
 let articles = document.querySelectorAll('.article');
 articles.forEach(article => {
   return new Article(article);
 })
-// console.log(article)
+console.log(articles)
