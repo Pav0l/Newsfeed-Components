f56071486096914d85a5da4b3ce4b4fa9e57b28b
Pav0l <pavol.karas@gmail.com>
2019-01-30 20:34:28 +0100

    Fix minor bug with article expand, DRY code

--- Article/Article.js
+++ Article/Article.js
@@ -18,71 +18,76 @@ class Article {
   }
 
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
+    getArticles(); // get the latest NodeList to loop through every article
+
+    // Loop through every article created and close expanded articles when you expand new article
     for (let i = 0; i < articles.length; i++) {
-      console.log(articles[i]);
       articles[i].classList.remove('article-open');      
     }
     this.domElement.classList.toggle('article-open');
   }
+
   closeArticle() {
     this.domElement.classList.remove('article-open');
   }
 }
 
-/* START HERE:
-- Select all classes named ".article" and assign that value to the articles variable.  
+const queSel = selector => document.querySelector(selector);
+const headerField = queSel('#title');
+const articleField = queSel('#articleText');
+const articlesDiv = queSel('.articles');
 
-- With your selection in place, now chain .forEach() on to the articles variable 
-to iterate over the articles NodeList and create a new instance of Article by passing 
-in each article as a parameter to the constructor.
-*/
-
-const headerField = document.querySelector('#title');
-const articleField = document.querySelector('#articleText');
-const articlesDiv = document.querySelector('.articles');
-
-
+// Function to create articles and return new Article class
 const createArticle = () => {
-  const newDiv = document.createElement('div');
-  newDiv.className = 'article';
-  articlesDiv.appendChild(newDiv);
+
+  const createDiv = (element, classname = "", text = "") => {
+    const newDiv = document.createElement(element);
+    newDiv.className = classname;
+    newDiv.textContent = text;
+    articlesDiv.appendChild(newDiv)
+    return newDiv
+  }
+
+  const createEl = (element, classname = "", text = "") => {
+    const newEl = document.createElement(element);
+    newEl.className = classname;
+    newEl.textContent = text;
+    newDiv.appendChild(newEl)
+  }
+
+  let newDiv = createDiv('div', 'article');
+  let newTitle = createEl('h2', "", headerField.value);
+  let newDate = createEl('p', 'date', '30th Jan 2019');
+  let newArticle = createEl('p', "", articleField.value);
+  let spanExpand = createEl('span', 'expandButton', 'expand');
+  let spanClose = createEl('span', 'closeButton', 'close');
   
-  const newTitle = document.createElement('h2');
-  newTitle.textContent = headerField.value;
-  newDiv.appendChild(newTitle);
-  
-  const newDate = document.createElement('p');
-  newDate.textContent = 'Jan 30th, 2019';
-  newDate.className = 'date';
-  newDiv.appendChild(newDate);
-  
-  const newArticle = document.createElement('p');
-  newArticle.textContent = articleField.value;
-  newDiv.appendChild(newArticle);
-  
-  const spanExpand = document.createElement('span');
-  spanExpand.className = 'expandButton';
-  spanExpand.textContent = 'expand';
-  newDiv.appendChild(spanExpand);
-  
-  const spanClose = document.createElement('span');
-  spanClose.className = 'closeButton';
-  spanClose.textContent = 'close';
-  newDiv.appendChild(spanClose);
-  
+// reset the text inside textfield after clicking Submit
   headerField.value = '';
   articleField.value = '';
 
   return new Article(newDiv);
 }
 
-const theButton = document.querySelector('#submitBtn');
+
+// Add functionality to Submit button to createArticle
+const theButton = queSel('#submitBtn');
 theButton.addEventListener('click', createArticle);
 
+// Select all classes named ".article" and assign that value to the articles variable. 
+// Make it a function so you can update the NodeList with latest articles
+let articles;
+const getArticles = () => {
+  return articles = document.querySelectorAll('.article');
+};
+getArticles();
 
-let articles = document.querySelectorAll('.article');
-articles.forEach(article => {
-  console.log(article)
-  return new Article(article);
-})
+/*
+With your selection in place, now chain .forEach() on to the articles variable 
+to iterate over the articles NodeList and create a new instance of Article by passing 
+in each article as a parameter to the constructor.
+*/
+articles.forEach(article => {return new Article(article);})
+
+
+console.log(articles)
